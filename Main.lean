86146402b09e8ee776fc743ba258b10507654b22
sasheen <chandra.uniii@gmail.com>
2026-09-18 15:51:06 -0700

    Completed up til 1.4.3 exercises #1

--- Main.lean
+++ Main.lean
@@ -1,4 +1,37 @@
 import LEANLEARNING
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+-- variables are declared using def. def [name] : [type] := [value]
+def test : String := "Hello world!"
+
+-- variables may be used as values in expressions
+#eval test
+
+-- functions are declared using def. def [name] [param : type] : [return type] := [func]
+def max (a : Nat) (b : Nat) : Nat :=
+  if a > b then a
+  else b
+
+-- Lean defaults to unsigned Natural numbers. Use int for negative calculations
+#eval (1 - 2 : Int)
+
+def joinStringsWith (joiner : String) (b : String) (c : String) : String :=
+  String.append b (String.append joiner c)
+
+#eval joinStringsWith ", " "one" "and another"
+
+def volume (height : Nat) (width : Nat) (depth : Nat) : Nat :=
+  height * width * depth
+
+#eval volume 10 20 30
+
+structure RectangularPrism where
+  height : Float
+  width : Float
+  depth : Float
+
+def volume2 (prism : RectangularPrism) : Float :=
+  prism.height * prism.width * prism.depth
+
+def prism : RectangularPrism := { height := 4.0, width := 2.0, depth := 1.0 }
+
+#eval volume2 prism
