0be7b458beb71e118e952f01c5e5a408f3ebce33
sasheen <chandra.uniii@gmail.com>
2026-09-24 11:45:08 -0700

    chore: move old practice code to separate file

---
+++ Old.lean
@@ -0,0 +1,73 @@
+import Mathlib
+
+-- variables are declared using def. def [name] : [type] := [value]
+def test : String := "Hello world!"
+
+
+
+-- variables may be used as values in expressions
+#eval test
+
+-- functions are declared using def. def [name] [param : type] : [return type] := [func]
+def max2 (a : Nat) (b : Nat) : Nat :=
+  if a > b then a
+  else b
+
+#eval max2 3 4
+
+-- Lean defaults to unsigned Natural numbers. Use int for negative calculations
+#eval (1 - 2 : Int)
+
+def joinStringsWith (joiner : String) (b : String) (c : String) : String :=
+  String.append b (String.append joiner c)
+
+#eval joinStringsWith ", " "one" "and another"
+
+def volume (height : Nat) (width : Nat) (depth : Nat) : Nat :=
+  height * width * depth
+
+#eval volume 10 20 30
+
+-- Structures, like in C!
+structure RectangularPrism where
+  height : Float
+  width : Float
+  depth : Float
+
+structure Square where
+  sideLength : Float
+
+def area (square : Square) : Float :=
+  square.sideLength * square.sideLength
+
+def square : Square := { sideLength := 8 }
+
+#eval area square
+
+def volume2 (prism : RectangularPrism) : Float :=
+  prism.height * prism.width * prism.depth
+
+-- Defining a structure is very similar to defining regular variables.
+-- It is important to use the curly-brace structure to define struct members.
+-- To define a structure in-line, eg. #check { height := 1.0, width := 1.0, length := 1.0}, you
+-- must include a definition of the type. This can be done easily as shown below:
+-- { height := 1.0, width := 1.0, length := 1.0 : RectangularPrism}
+def prism : RectangularPrism := { height := 4.0, width := 2.0, depth := 1.0 }
+
+#eval volume2 prism
+
+structure Segment where
+  extr1 : Float
+  extr2 : Float
+
+def length (lineSegment : Segment) : Float :=
+  lineSegment.extr2 - lineSegment.extr1
+
+def lineSeg : Segment := { extr1 := 1.0, extr2 := 5.0 }
+
+#eval length lineSeg
+
+--------------------------------------------------------------------------------------------------------------- --
+
+def main : IO Unit :=
+  IO.println "Hello, world!"
